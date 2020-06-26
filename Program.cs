4cd3cf6f9fae701a9a054c8ec80db17d60a7a27d
sandy-tartari <62676992+sandy-tartari@users.noreply.github.com>
2020-06-26 16:24:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumeroOrdemCrescente
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        { 
+            int [] listanumeros = new int  [5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite um número:");
+                listanumeros[i] = int.Parse(Console.ReadLine());
+               
+            }
+            Array.Sort(listanumeros);
+            foreach (int i in listanumeros)
+            {
+                Console.WriteLine("" + i);
+            }
+        }
+        
+    }
+} 
